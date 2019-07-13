f5fc7ae856e1fce77ff778c07a31cf3b1413dd1c
rachelroll <hai30@163.com>
2019-07-13 15:11:06 +0800

    update(passport)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,11 @@ namespace App\Providers;
 
 use App\Console\Commands\ModelMakeCommand;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\AuthCode;
+use Laravel\Passport\Client;
+use Laravel\Passport\Passport;
+use Laravel\Passport\PersonalAccessClient;
+use Laravel\Passport\Token;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +31,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Passport::routes();
+
+        Passport::tokensExpireIn(now()->addDays(15));
+
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+
+        Passport::useTokenModel(Token::class);
+        Passport::useClientModel(Client::class);
+        Passport::useAuthCodeModel(AuthCode::class);
+        Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
+
     }
 }
