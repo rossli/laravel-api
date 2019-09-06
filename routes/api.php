4d20db6414fc4ec5d1d89574436f57aee3827796
rachelroll <hai30@163.com>
2019-09-06 17:12:53 +0800

    update(payment)

--- routes/api.php
+++ routes/api.php
@@ -25,11 +25,10 @@ Route::namespace('Api')->prefix('v1')->middleware([\Barryvdh\Cors\HandleCors::cl
     Route::post('/auth/reset', 'AuthController@reset')->name('api.auth.reset');
     Route::post('/auth/sms-login', 'AuthController@smsLogin')->name('api.auth.sms-login');
 
-    Route::get('/user/exists','UserController@exists')->name('api.user.check-mobile');
+    Route::get('/user/exists', 'UserController@exists')->name('api.user.check-mobile');
     //验证码
-    Route::get('/user/captcha','UserController@captcha')->name('api.user.captcha');
-    Route::post('/user/sms-code','UserController@smsCode')->name('api.user.sms-code');
-
+    Route::get('/user/captcha', 'UserController@captcha')->name('api.user.captcha');
+    Route::post('/user/sms-code', 'UserController@smsCode')->name('api.user.sms-code');
 
     Route::get('/banners', 'BannerController@index')->name('api.banners.index');
     Route::get('/course/list', 'CourseController@list')->name('api.course.list');
@@ -43,6 +42,7 @@ Route::namespace('Api')->prefix('v1')->middleware([\Barryvdh\Cors\HandleCors::cl
     Route::get('/course/task/live', 'CourseTaskController@live')->name('api.course-task.live');
     Route::get('/course/task/video', 'CourseTaskController@video')->name('api.course-task.video');
     Route::get('/course/material/{id}', 'CourseMaterialController@show')->name('api.course-material.show');
+
     Route::middleware('auth:api')->group(function () {
         Route::get('/users', 'UserController@index')->name('api.users.index');
         Route::post('/users/update/name', 'UserController@updateName')->name('api.users.update.name');
@@ -55,21 +55,28 @@ Route::namespace('Api')->prefix('v1')->middleware([\Barryvdh\Cors\HandleCors::cl
             Route::get('/order', 'MeController@order')->name('api.me.order');
             Route::get('/is-student/{id}', 'MeController@isStudent')->name('api.me.isStudent');
         });
-        Route::get('/order/book-submit', 'OrderController@bookSubmit')->name('api.order.book-submit');
-        Route::get('/order/course-submit', 'OrderController@courseSubmit')->name('api.order.course-submit');
-        Route::get('/order/cart-submit', 'OrderController@cartSubmit')->name('api.order.cart-submit');
-        Route::get('/order/confirm', 'OrderController@confirm')->name('api.order.confirm');
-        Route::get('/order/cancel/{id}', 'OrderController@cancel')->name('api.order.cancel');
-        Route::get('/order/{id}', 'OrderController@show')->name('api.order.show');
-        Route::post('/cart/store', 'ShoppingCartController@store')->name('api.cart.store');
-        Route::get('/cart', 'ShoppingCartController@index')->name('api.cart.index');
-        Route::get('/cart/count', 'ShoppingCartController@count')->name('api.cart.count');
-        Route::post('/cart/delete', 'ShoppingCartController@delete')->name('api.cart.delete');
-        Route::get('/payment-wx','OrderController@paymentWx')->name('api.order.payment-wx');
-        Route::get('/payment-h5','OrderController@paymentH5')->name('api.order.payment-h5');
-        Route::post('/payment/notify', 'PaymentController@notify')->name('api.payment.notify');
-        //查询支付状态
-        Route::get('/payment/status/{id}', 'PaymentController@status')->name('web.payment.status');
+
+        Route::prefix('order')->group(function () {
+            Route::get('/book-submit', 'OrderController@bookSubmit')->name('api.order.book-submit');
+            Route::get('/course-submit', 'OrderController@courseSubmit')->name('api.order.course-submit');
+            Route::get('/cart-submit', 'OrderController@cartSubmit')->name('api.order.cart-submit');
+            Route::get('/confirm', 'OrderController@confirm')->name('api.order.confirm');
+            Route::get('/cancel/{id}', 'OrderController@cancel')->name('api.order.cancel');
+            Route::get('/payment-wx', 'OrderController@paymentWx')->name('api.order.payment-wx');
+            Route::get('/payment-h5', 'OrderController@paymentH5')->name('api.order.payment-h5');
+
+            Route::get('/{id}', 'OrderController@show')->name('api.order.show');
+
+
+        });
+
+        Route::prefix('cart')->group(function () {
+
+            Route::post('/store', 'ShoppingCartController@store')->name('api.cart.store');
+            Route::get('/', 'ShoppingCartController@index')->name('api.cart.index');
+            Route::get('/count', 'ShoppingCartController@count')->name('api.cart.count');
+            Route::post('/delete', 'ShoppingCartController@delete')->name('api.cart.delete');
+        });
 
     });
 
