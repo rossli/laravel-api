5e3c1e3402ebef74a979f3d13690af0310aed063
xue <1782184089@qq.com>
2019-09-18 16:10:42 +0800

    update

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -45,7 +45,7 @@ class BookController extends BaseController
 
     public function list()
     {
-        $books = Book::where('enabled', 1)->orderBy('updated_at', 'DESC')->limit(6)->get();
+        $books = Book::where('enabled', 1)->orderBy('updated_at', 'DESC')->get();
         $data = [];
         $books->each(function ($item) use (&$data) {
             $data[] = [
